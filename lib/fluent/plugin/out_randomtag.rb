f22fda706ccaae72c25f3240a2d9840918b3cfbf
David Winiarski <david@go2mobi.com>
2016-04-29 11:00:28 -0700

    New Fluentd Plugin Random Tag

--- lib/fluent/plugin/out_randomtag.rb
+++ lib/fluent/plugin/out_randomtag.rb
@@ -2,26 +2,25 @@
 require "securerandom"
 
 module Fluent
-  class RandomOutput < Output
+  class RandomTagOutput < Output
     include Fluent::HandleTagNameMixin
 
-    Fluent::Plugin.register_output('random', self)
+    Fluent::Plugin.register_output('random_tag', self)
 
     config_param :integer, :string, :default => nil
-    config_param :field, :string, :default => nil
 
 
     def parse_integer_field(interval)
       lower_upper = interval.split("..", 2).map{|value| Integer(value) }
       if (lower_upper.length != 2 || !lower_upper[0].is_a?(Integer) || !lower_upper[1].is_a?(Integer))
-        raise "random: Bad value for 'integer' (value: '#{interval}')"
+        raise "random_tag: Bad value for 'integer' (value: '#{interval}')"
       end
       return lower_upper
     end
 
     def check_valid_interval(lower, upper)
       if (lower >= upper)
-        raise "random: Bad interval for 'integer' (#{lower} >= #{upper})"
+        raise "random_tag: Bad interval for 'integer' (#{lower} >= #{upper})"
       end
     end
 
@@ -29,25 +28,23 @@ module Fluent
     def configure(conf)
       super
 
-      if (field.nil? || integer.nil?)
-        raise ConfigError, "random: 'field' is required to be set."
-      end
-
       if (integer.nil?)
-        raise ConfigError, "random: 'integer' is required to be set."
+        raise ConfigError, "random_tag: 'integer' is required to be set."
       end
 
       @integer_interval = parse_integer_field(integer)
       check_valid_interval(@integer_interval[0], @integer_interval[1])
-      @field = field
       @random  = Random.new()
     end
 
     def emit(tag, es, chain)
       es.each { |time, record|
         t = tag.dup
-        filter_record(t, time, record)
-        Engine.emit(t, time, record)
+        r = process()
+        #Create new tag with random number prefixed
+        nT = "#{r}.#{t}"
+        filter_record(nT, time, record)
+        Engine.emit(nT, time, record)
       }
 
       chain.next
@@ -57,7 +54,6 @@ module Fluent
 
     def filter_record(tag, time, record)
       super(tag, time, record)
-      record[@field] = process()
     end
 
     def process()
